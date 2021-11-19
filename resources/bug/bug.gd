b626c75c8a290c80c9364329f7702c6610aa75cc
BESANCON Vincent <vincent.besancon@faurecia.com>
2021-11-19 20:09:17 +0100

    feat(bug): bugs are intelligents, finally !!

--- resources/bug/bug.gd
+++ resources/bug/bug.gd
@@ -1,73 +1,45 @@
 extends KinematicBody2D
 
-var velocity: Vector2 = Vector2.ZERO
-var direction: Vector2 = Vector2.RIGHT
-var max_speed: int = 200
-var jitter_force: int = 100
+var velocity: Vector2
+var aim: Vector2 = Vector2.RIGHT
 
-onready var MovementLine = Line2D.new()
-onready var CollisionLine = Line2D.new()
-onready var Heading = get_node("Heading")
-onready var Sprite = get_node("Heading/Sprite")
+var max_speed: int = 100
+var max_steering: float = 2.5
 
 #-------------------------------------------------------------------------------
 # Parent methods
 #-------------------------------------------------------------------------------
 
-func _ready():
-    add_child(MovementLine)
-    add_child(CollisionLine)
-
-    MovementLine.default_color = ColorN("red")
-    MovementLine.width = 2
-    CollisionLine.default_color = ColorN("green")
-    CollisionLine.width = 2
-
 func _physics_process(delta):
-    update_debug_vectors()
-    update_position(delta)
+    var steering: Vector2 = Vector2.ZERO
+
+    steering += seek_steering()
+    steering = steering.clamped(max_steering)
+
+    velocity += steering
+    velocity = velocity.clamped(max_speed)
+    velocity = velocity.rotated(rand_range(-0.1, 0.1))
+
+    rotation = velocity.angle()
+
+    var collision = move_and_collide(velocity * delta)
+    if collision:
+        var normal: Vector2 = collision.normal
+        var part: CollisionShape2D = collision.local_shape
+        steering = steering.slide(normal)
+
+        if part.name == "HeadCollision":
+            aim = steering.bounce(normal)
+
+        move_and_collide(steering * delta)
 
 #-------------------------------------------------------------------------------
 # Instance methods
 #-------------------------------------------------------------------------------
 
-func update_debug_vectors():
-    MovementLine.clear_points()
-    CollisionLine.clear_points()
-    MovementLine.update()
-    CollisionLine.update()
-
-func update_position(delta):
-    var jitter_angle: float = rand_range(-2, 2)
-    var target: Vector2 = direction.normalized() * max_speed
-
-    var collision: KinematicCollision2D = move_and_collide(velocity * delta)
-
-    velocity = velocity.linear_interpolate(target, delta)
-    velocity = velocity.clamped(max_speed)
-    velocity = velocity.rotated(lerp_angle(0, deg2rad(jitter_angle * jitter_force), delta))
-    rotation = velocity.angle()
-
-    MovementLine.add_point(Vector2.ZERO)
-    MovementLine.add_point(Vector2(velocity.length(), 0))
-
-    if collision:
-        var body: StaticBody2D = collision.get_collider()
-        var part: CollisionShape2D = collision.local_shape
-        var normal: Vector2 = collision.normal
-        var remainder: Vector2 = collision.remainder
-        var slide = velocity.slide(normal)
-
-        if body.is_in_group("obstacles") and part.name == "HeadCollision":
-            var reflect = remainder.bounce(normal)
-            var new_direction: Vector2 = reflect.normalized()
-            direction = new_direction
-
-            CollisionLine.add_point(Vector2.ZERO)
-            CollisionLine.add_point(direction * 100)
-
-        # warning-ignore:return_value_discarded
-        move_and_collide(slide * delta)
+func seek_steering() -> Vector2:
+    var desired_velocity: Vector2 = aim.normalized() * max_speed
+    return desired_velocity - velocity
 
 #-------------------------------------------------------------------------------
 # Connected signals
